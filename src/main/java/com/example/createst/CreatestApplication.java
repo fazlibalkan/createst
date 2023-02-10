fc3696e986a5f608b7face4769adf7a0fe2de43f
fazlibalkan <fazlibalkan@outlook.com>
2023-02-10 20:24:53 +0300

    init

---
+++ src/main/java/com/example/createst/CreatestApplication.java
@@ -0,0 +1,13 @@
+package com.example.createst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/createst/CreatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.createst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
